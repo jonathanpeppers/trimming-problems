2decf66cb5431f4af3356de7f52f29ee6a662d43
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-03-05 10:54:22 -0600

    Update Program.cs

--- trimming-problems/Program.cs
+++ trimming-problems/Program.cs
@@ -2,12 +2,13 @@
 
 const int length = 3;
 
-var array1 = JavaArrayCreator.Create<JavaObject>(length);
-Console.WriteLine(array1.Length);
-foreach (var item in array1)
-{
-	Console.WriteLine(item is null);
-}
+// Uncomment will fix expression below
+// var array1 = JavaArrayCreator.Create<JavaObject>(length);
+// Console.WriteLine(array1.Length);
+// foreach (var item in array1)
+// {
+// 	Console.WriteLine(item is null);
+// }
 
 var array2 = JavaArrayCreator.Create(typeof(JavaObject[]), length);
 Console.WriteLine(array2.Length);
@@ -15,3 +16,12 @@ foreach (var item in array2)
 {
 	Console.WriteLine(item is null);
 }
+
+// Another example:
+// var empty = new JavaObject[length];
+// JavaArrayCreator.Fill(empty);
+// Console.WriteLine(empty.Length);
+// foreach (var item in empty)
+// {
+// 	Console.WriteLine(item is null);
+// }
